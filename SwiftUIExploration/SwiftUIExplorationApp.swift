c4f212f4a1cb22b190a047c4dad60025ee44a71e
Trevor Miller <5497885+trevordmiller@users.noreply.github.com>
2023-03-14 11:57:18 -0600

    Initial Commit

---
+++ SwiftUIExploration/SwiftUIExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExplorationApp.swift
+//  SwiftUIExploration
+//
+//  Created by Trevor Miller on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

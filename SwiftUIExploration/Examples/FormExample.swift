71866f99706939d7ae8b31b18afda81b2ab35f42
Trevor Miller <5497885+trevordmiller@users.noreply.github.com>
2023-03-14 14:04:20 -0600

    Add Form

---
+++ SwiftUIExploration/Examples/FormExample.swift
@@ -0,0 +1,22 @@
+//
+//  FormExample.swift
+//  SwiftUIExploration
+//
+//  Created by Trevor Miller on 3/14/23.
+//
+
+import SwiftUI
+
+struct FormExample: View {
+    var body: some View {
+        Form {
+            Text("Content")
+        }
+    }
+}
+
+struct FormExample_Previews: PreviewProvider {
+    static var previews: some View {
+        FormExample()
+    }
+}

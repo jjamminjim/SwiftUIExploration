16f6631e1f6e7b3e895bffd80f48897738df16a9
Trevor Miller <5497885+trevordmiller@users.noreply.github.com>
2023-03-14 12:49:32 -0600

    Add GroupBox

---
+++ SwiftUIExploration/Examples/GroupBoxExample.swift
@@ -0,0 +1,22 @@
+//
+//  GroupBoxExample.swift
+//  SwiftUIExploration
+//
+//  Created by Trevor Miller on 3/14/23.
+//
+
+import SwiftUI
+
+struct GroupBoxExample: View {
+    var body: some View {
+        GroupBox(label: Text("GroupBox")) {
+            Text("Content")
+        }
+    }
+}
+
+struct GroupBoxExample_Previews: PreviewProvider {
+    static var previews: some View {
+        GroupBoxExample()
+    }
+}

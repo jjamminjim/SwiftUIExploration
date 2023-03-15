0d60433538f1aa72d8451191476db71066c997f4
Trevor Miller <5497885+trevordmiller@users.noreply.github.com>
2023-03-15 11:25:55 -0600

    Add swipeActions

---
+++ SwiftUIExploration/Examples/SwipeActionsExample.swift
@@ -0,0 +1,37 @@
+//
+//  SwipeActionsExample.swift
+//  SwiftUIExploration
+//
+//  Created by Trevor Miller on 3/15/23.
+//
+
+import SwiftUI
+
+struct SwipeActionsExample: View {
+    var body: some View {
+        List {
+            Text("SwipeActions")
+                .swipeActions(edge: .leading) {
+                        Button {} label: {
+                            Label("Label", systemImage: "star")
+                        }
+                    }
+                    .swipeActions(edge: .trailing) {
+                        Button(role: .destructive) {} label: {
+                            Label("Label", systemImage: "trash")
+                        }
+                        
+                        Button {} label: {
+                            Label("Label", systemImage: "flag")
+                        }
+                        .tint(.orange)
+                    }
+        }
+    }
+}
+
+struct SwipeActionsExample_Previews: PreviewProvider {
+    static var previews: some View {
+        SwipeActionsExample()
+    }
+}
